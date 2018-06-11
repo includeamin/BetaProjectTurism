a920d0c64018179670751764617901d426b87cb8
amin jamal <aminjamal10@gmail.com>
2018-06-11 07:31:03 -0700

    add

---
+++ AbolHasani/Server/Classes/Tools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Classes
+{
+    public static class Tools
+    {
+        public static void GenerateCode()
+        {
+
+        }
+    }
+}
